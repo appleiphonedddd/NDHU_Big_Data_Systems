75656022e0ff3250979b4324486524fcd2a85fcf
appleiphonedddd <611221201@gms.ndhu.edu.tw>
2025-01-08 14:22:11 +0800

    Final Project

--- Final_Project/deploy_files.sh
+++ Final_Project/deploy_files.sh
@@ -4,3 +4,4 @@ docker cp data.csv jobmanager:/opt/flink
 docker cp find_max_rainfall.py jobmanager:/opt/flink
 docker cp heavy_rain_analysis.py jobmanager:/opt/flink
 docker cp average_temperature_trend.py jobmanager:/opt/flink
+docker cp temperature_humidity_plot.py jobmanager:/opt/flink
